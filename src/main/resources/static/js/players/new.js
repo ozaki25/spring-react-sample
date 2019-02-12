e3d4fbe852dbe61b393c9caf4cfb06d6bd7d0623
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-12 23:36:08 +0900

    add intractive form

--- src/main/resources/static/js/players/new.js
+++ src/main/resources/static/js/players/new.js
@@ -2,29 +2,50 @@ class PlayerForm extends React.Component {
   state = {
     name: '',
     age: '',
+    league: '',
     team: '',
     position: '',
+    teams: [],
+  };
+
+  fetchTeams = async league => {
+    try {
+      const teams = await fetch(`/api/teams?league=${league}`).then(res => res.json());
+      this.setState({ teams });
+    } catch (e) {
+      alert(e.toString());
+    }
+  };
+
+  invalid = () => {
+    const { name, age, league, team, position } = this.state;
+    return (
+      name.trim() === '' ||
+      age.trim() === '' ||
+      league.trim() === '' ||
+      team.trim() === '' ||
+      position.trim() === ''
+    );
   };
 
   onChange = event => this.setState({ [event.target.name]: event.target.value });
 
+  onChangeLeague = event => {
+    this.onChange(event);
+    this.fetchTeams(event.target.value);
+  };
+
   onSubmit = event => {
-    console.log(this.state);
     event.preventDefault();
     if (this.invalid()) {
-      alert('入力内容を確認して下さい');
+      alert('未入力の項目があります');
     } else {
       event.target.submit();
     }
   };
 
-  invalid = () => {
-    const { name, age, team, position } = this.state;
-    return name.trim() === '' || age.trim() === '' || team.trim() === '' || position.trim() === '';
-  };
-
   render() {
-    const { name, age, team, position } = this.state;
+    console.log({ name, age, league, team, position, teams });
     return (
       <form action="/players" method="post" onSubmit={this.onSubmit}>
         <div className="form-group">
@@ -48,22 +69,55 @@ class PlayerForm extends React.Component {
           />
         </div>
         <div className="form-group">
-          <label className="control-label">チーム名</label>
-          <input
-            className="form-control"
-            type="text"
-            name="team"
-            team={team}
-            onChange={this.onChange}
-          />
+          <label className="control-label">リーグ</label>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="league"
+                id="league-npb"
+                value="npb"
+                onChange={this.onChangeLeague}
+              />
+              <label className="form-check-label" htmlFor="league-npb">
+                NPB
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="league"
+                id="league-mlb"
+                value="mlb"
+                onChange={this.onChangeLeague}
+              />
+              <label className="form-check-label" htmlFor="league-mlb">
+                MLB
+              </label>
+            </div>
+          </div>
         </div>
+        {league && teams.length && (
+          <div className="form-group">
+            <label className="control-label">チーム</label>
+            <select className="form-control" name="team" value={team} onChange={this.onChange}>
+              {teams.map((team, key) => (
+                <option key={key} value={team}>
+                  {team}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
         <div className="form-group">
           <label className="control-label">守備位置</label>
           <input
             className="form-control"
             type="text"
             name="position"
-            position={position}
+            value={position}
             onChange={this.onChange}
           />
         </div>
