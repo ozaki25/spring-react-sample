a6ed8acc8261d331db9ef2f1eac1b61b3fd8dd9c
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-13 00:42:08 +0900

    refactor and use hooks

--- src/main/resources/static/js/players/index.js
+++ src/main/resources/static/js/players/index.js
@@ -1,3 +1,25 @@
+function fetchPlayers() {
+  try {
+    return fetch('/api/players').then(res => res.json());
+  } catch (e) {
+    alert(e.toString());
+  }
+}
+
+function deletePlayer(id) {
+  try {
+    return fetch(`/api/players/${id}`, { method: 'DELETE' });
+  } catch (e) {
+    alert(e.toString());
+  }
+}
+
+function sorting(list, col, desc) {
+  return list.sort((a, b) =>
+    desc ? (a[col] > b[col] ? -1 : 1) : b[col] > a[col] ? -1 : 1,
+  );
+}
+
 function PlayerItem({
   id,
   name,
@@ -43,89 +65,60 @@ function PlayerItem({
   );
 }
 
-const sort = (list, col, desc) =>
-  list.sort((a, b) =>
-    desc ? (a[col] > b[col] ? -1 : 1) : b[col] > a[col] ? -1 : 1,
+function PlayerList() {
+  const [players, setPlayers] = React.useState(null);
+  const [sort, setSort] = React.useState({ col: 'id', desc: false });
+
+  React.useEffect(() => {
+    updatePlayers();
+  }, []);
+
+  const updatePlayers = async () => setPlayers(await fetchPlayers());
+
+  const onClickShow = id => (location.href = `/players/${id}`);
+
+  const onClickEdit = id => (location.href = `/players/${id}/edit`);
+
+  const onClickDelete = async id => {
+    await deletePlayer(id);
+    updatePlayers();
+  };
+
+  const sortBy = col => {
+    const desc = sort.col === col ? !sort.desc : false;
+    setPlayers(sorting(players, col, desc));
+    setSort({ col, desc });
+  };
+
+  return players ? (
+    <table className="table">
+      <thead>
+        <tr>
+          <th onClick={() => sortBy('id')}>ID</th>
+          <th onClick={() => sortBy('name')}>名前</th>
+          <th onClick={() => sortBy('age')}>年齢</th>
+          <th onClick={() => sortBy('team')}>チーム名</th>
+          <th onClick={() => sortBy('position')}>守備位置</th>
+          <th />
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {players.map(props => (
+          <PlayerItem
+            key={props.id}
+            onClickShow={onClickShow}
+            onClickEdit={onClickEdit}
+            onClickDelete={onClickDelete}
+            {...props}
+          />
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    <p>Loading...</p>
   );
-
-class PlayerList extends React.Component {
-  state = {
-    players: null,
-    sort: {
-      col: 'id',
-      desc: false,
-    },
-  };
-
-  componentDidMount() {
-    this.fetchPlayers();
-  }
-
-  fetchPlayers = async () => {
-    try {
-      const players = await fetch('/api/players').then(res => res.json());
-      this.setState({ players });
-    } catch (e) {
-      alert(e.toString());
-    }
-  };
-
-  onClickShow = id => (location.href = `/players/${id}`);
-
-  onClickEdit = id => (location.href = `/players/${id}/edit`);
-
-  onClickDelete = async id => {
-    try {
-      await fetch(`/api/players/${id}`, { method: 'DELETE' });
-      this.fetchPlayers();
-    } catch (e) {
-      alert(e.toString());
-    }
-  };
-
-  sortBy = col => {
-    this.setState(prevState => {
-      const desc = prevState.sort.col === col ? !prevState.sort.desc : false;
-      return {
-        players: sort(prevState.players, col, desc),
-        sort: { col, desc },
-      };
-    });
-  };
-
-  render() {
-    const { players } = this.state;
-    console.log(players);
-    return players ? (
-      <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.sortBy('id')}>ID</th>
-            <th onClick={() => this.sortBy('name')}>名前</th>
-            <th onClick={() => this.sortBy('age')}>年齢</th>
-            <th onClick={() => this.sortBy('team')}>チーム名</th>
-            <th onClick={() => this.sortBy('position')}>守備位置</th>
-            <th />
-            <th />
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {players.map(props => (
-            <PlayerItem
-              key={props.id}
-              onClickShow={this.onClickShow}
-              onClickEdit={this.onClickEdit}
-              onClickDelete={this.onClickDelete}
-              {...props}
-            />
-          ))}
-        </tbody>
-      </table>
-    ) : (
-      <p>Loading...</p>
-    );
-  }
 }
 
 ReactDOM.render(<PlayerList />, document.getElementById('react-root'));
